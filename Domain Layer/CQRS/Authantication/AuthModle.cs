2c2d6532cdb197a652ae31109fac2a3699846ba9
Eng Khaled <Eng Khaled@DESKTOP-9QN4FUV>
2025-12-02 22:00:30 +0200

    a

---
+++ Domain Layer/CQRS/Authantication/AuthModle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain_Layer.CQRS.Authantication
+{
+    public class AuthModle
+    {
+        public bool IsAuthenticated { get; set; }
+
+        public string? Token { get; set; } = default!;
+        public string? RefreshToken { get; set; } = default!;
+
+         public DateTime? TokenExpiration { get; set; }
+
+        public DateTime? RefreshTokenExpiration { get; set; }
+    }
+}
