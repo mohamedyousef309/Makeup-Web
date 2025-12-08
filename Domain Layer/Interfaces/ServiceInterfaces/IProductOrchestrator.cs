91defef8b62290f151d3695e7985e2992f2407b9
mohamedyousef309 <m.ayousef2003@gmail.com>
2025-12-08 05:46:53 +0200

    edits of product service

---
+++ Domain Layer/Interfaces/ServiceInterfaces/IProductOrchestrator.cs
@@ -0,0 +1,19 @@
+﻿using Domain_Layer.DTOs.ProductDtos;
+using Domain_Layer.Respones;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain_Layer.Interfaces.ServiceInterfaces
+{
+    public interface IProductOrchestrator
+    {
+        Task<RequestRespones<ProductDto>> CreateProduct(CreateProductDto dto);
+        Task<RequestRespones<ProductDto>> UpdateProduct(UpdateProductDto dto);
+        Task<RequestRespones<bool>> DeleteProduct(int id);
+        Task<RequestRespones<List<ProductDto>>> GetAllProducts();
+        Task<RequestRespones<ProductDto>> GetProductById(int id);
+    }
+}
